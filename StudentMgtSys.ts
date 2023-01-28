c7cbbc539516be2000371102d7c3100831a3497d
MuhammadYasirSiddique <111221213+MuhammadYasirSiddique@users.noreply.github.com>
2023-01-29 02:26:30 +0500

    Add files via upload

--- StudentMgtSys.ts
+++ StudentMgtSys.ts
@@ -13,13 +13,23 @@ class Student {
     registerForCourse(course: Course) {
         this.courses.push(course);
     }
+
+    getname(): string {
+        return this.name
+    }
+}
+
+interface FeeRecord {
+    studentId: number;
+    amount: number;
+    month: string;
 }
 
 class Fee extends Student{
     feeAmount: number;
     feeMonth: string;
     studentId: number;
-    feeRecords: object[];
+    feeRecords: FeeRecord[];
 
     constructor(name: string, id: number) {
         super(name, id);
@@ -27,14 +37,14 @@ class Fee extends Student{
         this.feeRecords = [];
     }
 
-    inputFee(amount: number, month: string) {
+    inputFee(id:number, amount: number, month: string) {
         this.feeAmount = amount;
         this.feeMonth = month;
         this.feeRecords.push({studentId: this.studentId, amount: this.feeAmount, month: this.feeMonth});
     }
-
-    getFeeRecord(studentId: number) {
-        let records = this.feeRecords.filter((record) => record.studentId === studentId);
+    
+    public getFeeRecord(studentId: number) {
+        let records = this.feeRecords.filter((record:FeeRecord) => record.studentId === studentId);
         return records;
     }
 }
@@ -82,41 +92,55 @@ class StudentManagementSystem {
         this.courses.push(course);
         return course;
     }
+
+    getStudentList(): string[] {
+        return this.students.map(student=> student.name);
+    }   
 }
 
 
 let sms = new StudentManagementSystem();
-let student1 = sms.addStudent("John", 123);
-let student2 = sms.addStudent("Jane", 456);
-let course1 = sms.addCourse("Math", "MTH101");
-let course2 = sms.addCourse("English", "ENG101");
+let student1 = sms.addStudent("Yasir", 123) as Fee;
+let student2 = sms.addStudent("Nasir", 456) as Fee;
+let student3 = sms.addStudent("Jafar", 2255) as Fee;
+let course1 = sms.addCourse("Internet of Things", "IOT101");
+let course2 = sms.addCourse("Blockchain", "BCC102");
+let course3 = sms.addCourse("Artificial Intelligence", "AIC103")
 
 sms.registerStudentForCourse(student1, course1);
 sms.registerStudentForCourse(student1, course2);
+sms.registerStudentForCourse(student1, course3);
 sms.registerStudentForCourse(student2, course1);
 
-console.log("Student 1 registered courses:")
+console.log(`${student1.name} registered courses`)
 student1.courses.forEach((course) => {
     console.log(course.title);
 });
 
-console.log("\nStudent 2 registered courses:")
+console.log(`\n${student2.name} registered courses:`)
 student2.courses.forEach((course) => {
     console.log(course.title);
 });
 
-student1.inputFee(1000, "January");
-student1.inputFee(2000, "February");
-student2.inputFee(3000, "January");
+student1.inputFee(1, 1000, "January");
+student1.inputFee(2, 2000, "February");
+student2.inputFee(3, 3000, "January");
 
-console.log("\nStudent 1 fee records:")
+console.log(`\n${student1.name}  fee records:`)
 let student1FeeRecords = student1.getFeeRecord(student1.studentId);
 student1FeeRecords.forEach((record) => {
-    console.log(`Amount: ${record.amount}, Month: ${record.month}`);
+    if ('amount' in record  &&'month' in record) {
+        console.log(`Amount: ${record.amount}, Month: ${record.month}`);
+    }
 });
 
-console.log("\nStudent 2 fee records:")
+console.log(`\n ${student2.name} fee records:`)
 let student2FeeRecords = student2.getFeeRecord(student2.studentId);
 student2FeeRecords.forEach((record) => {
-    console.log(`Amount: ${record.amount}, Month: ${record.month}`);
+    if ('amount' in record && 'month' in record) {
+        console.log(`Amount: ${record.amount}, Month: ${record.month}`);
+    }
 });
+
+console.log("\n List of registered students")
+console.log(sms.getStudentList());
